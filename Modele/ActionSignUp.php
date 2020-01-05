13b571debc2cc9e96713b35313881fffe5f153a3
Tbeaumont79 <tbeaumont79@icloud.com>
2020-01-05 20:48:00 +0100

    adding sqlsignup statement

--- Modele/ActionSignUp.php
+++ Modele/ActionSignUp.php
@@ -3,16 +3,22 @@
         private $_record;
         private $_linkToDb;
         private $_sqlStatementSignUp;
+        private $_success;
 
         public function __construct($email, $password) {
           //  $this->_sqlStatementSignUp = new SqlStatementSignUp();
             $this->_linkToDb = new ConnectToDb();
             $this->_linkToDb->connectToDb();
             $this->_record = new DataUserRecord(null, $password, $email);
+            $this->_sqlStatementSignUp = new SqlStatementSignup($this->_linkToDb, $this->_record);
         }
 
         public function actionSignUp() {
-
+            //check before if user already exist
+            $this->_sqlStatementSignUp->prepare();
+            $this->_sqlStatementSignUp->bindParam();
+            $this->_sqlStatementSignUp->execute();
+            $this->_success = $this->_sqlStatementSignUp->getExecuteSuccess();
         }
     }
 ?>
---
+++ lib/SqlStatement/SqlStatementSignUp.php
@@ -0,0 +1,16 @@
+<?php
+    class SqlStatementSignUp extends SqlStatement {
+        private $_sql;
+
+        public function __construct($linkToDb, $record) {
+            parent::__construct($linkToDb, $record);
+            $this->_sql = "INSERT VALUES(user,pass,email) INTO user WHERE (user=?, pass=?, email=?)";
+        }
+
+        public function bindParam() {
+            $this->_sql->bindParam(':user', $this->_record->getUsername());
+            $this->_sql->bindParam(':pass', $this->_record->getHashedPass());
+            $this->_sql->bindParam(':email', $this->_record->getEmail());
+        }
+    }
+?>
